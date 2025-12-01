f0758cbf597910063aff834b57b62538ae0ede22
ChatHongPT <suk924600@naver.com>
2025-12-01 19:14:34 +0900

    [feat] #3 - 라우터 설정

---
+++ src/router/Router.tsx
@@ -0,0 +1,14 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { routerList } from "./RouterList";
+import { Suspense } from "react";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+}
+
+export default Router;
